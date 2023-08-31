9e3505d7f142ec72166f6340adffb3598b24de7e
prasanth <prasanth3291@gmail.com>
2023-08-31 19:40:37 +0530

    1st target
    product,category(without crud),login using user verification gmail

---
+++ voguevilla/store/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render,get_object_or_404
+from.models import Product
+from category.models import category
+
+def store(request,category_slug=None):
+    categories=None
+    products=None
+    
+    if category_slug !=None:
+        categories=get_object_or_404(category,slug=category_slug)
+        products=Product.objects.all().filter(category=categories,is_available=True)
+        product_count=products.count()
+    else:    
+    
+        products=Product.objects.all().filter(is_available=True)        
+        product_count=products.count()
+        
+        
+    context= {
+        'products':products,
+        'product_count':product_count
+    }
+    return render(request,('store/store.html'),context)
+
+def product_detail(request,category_slug,product_slug):
+    return render(request,'store/product_detail.html')
