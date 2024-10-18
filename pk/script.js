966b1e73e45fa3d089dc2499177127d14abf72c0
Prasanth Kizhakkedath <prasanth3291@gmail.com>
2024-10-18 13:37:35 +0200

    Add portfolio page

--- pk/script.js
+++
@@ -1,5 +0,0 @@
-const name=document.getElementById('name')
-const password=document.getElementById('password')
-const form=document.getElementById('form')
-
-form.addEventListener('submit', (e) => {e.preventDefault()})
--- voguevilla/registration/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def register(request):
-    return render(request,"index.html")
-
-def signin(request):
-    return render(request,"signin.html")
-# Create your views here.
